3f65b711d1965f4630fea8b7336145bff2db6b69
unknown <rogerpantilyowger@gmail.com>
2023-09-24 11:46:24 +0800

    added sidebar active link

--- src/components/layout/sidebar/SidebarItem.tsx
+++ src/components/layout/sidebar/SidebarItem.tsx
@@ -1,7 +1,8 @@
 "use client"
 
+import { useRouter, usePathname } from "next/navigation"
 import { LucideIcon } from "lucide-react"
-import { useRouter } from "next/navigation"
+import clsx from "clsx"
 
 type Props = {
     label: string
@@ -11,6 +12,8 @@ type Props = {
 
 export default function SidebarItem({ label, Icon, href }: Props) {
     const router = useRouter()
+    const currentRoute = usePathname()
+    const isCurrentRoute = currentRoute === href
 
     const handleClick = () => {
         if (href) {
@@ -26,7 +29,14 @@ export default function SidebarItem({ label, Icon, href }: Props) {
 
             <div className="relative hidden md:flex items-row gap-4 py-2 cursor-pointer items-center">
                 <Icon />
-                <p className="hidden md:block text-xl">{label}</p>
+                <p
+                    className={clsx(
+                        isCurrentRoute && "font-semibold",
+                        "hidden md:block text-xl"
+                    )}
+                >
+                    {label}
+                </p>
             </div>
         </div>
     )
