d43f8e04a972a8053cfe79d6741656e534b97c04
unknown <rogerpantilyowger@gmail.com>
2023-09-24 11:34:40 +0800

    added not found page and fixed layout

--- src/components/layout/sidebar/Sidebar.tsx
+++ src/components/layout/sidebar/Sidebar.tsx
@@ -5,7 +5,7 @@ import SidebarItem from "./SidebarItem"
 
 export default function Sidebar() {
     return (
-        <div className="flex flex-col pt-5 space-y-2">
+        <div className="flex flex-col pt-16 space-y-2">
             <SidebarItem label="Home" Icon={Home} href="/" />
             <SidebarItem label="Profile" Icon={User2} href="/profile" />
             <SidebarItem label="Notifications" Icon={Bell} />
