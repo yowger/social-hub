2e77ae72522afe32b16330e862a0c31300ab7984
unknown <rogerpantilyowger@gmail.com>
2023-09-28 14:35:02 +0800

    dropdown update

--- src/components/layout/Header/UserProfileDown.tsx
+++ src/components/layout/Header/UserProfileDown.tsx
@@ -11,28 +11,34 @@ import {
     DropdownMenuGroup,
     DropdownMenuItem,
     DropdownMenuSeparator,
-    DropdownMenuShortcut,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { signOut } from "next-auth/react"
 
 export default function UserProfileDown({ session }: { session: Session }) {
+    const handleSignOut = async () => {
+        console.log("sign out")
+        await signOut()
+    }
     return (
         <>
             {session?.user ? (
                 <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
+                    <DropdownMenuTrigger
+                        asChild
+                        className="cursor-pointer hover:brightness-125 active:brightness-150 duration-200"
+                    >
                         <Avatar>
                             <AvatarImage src="https://github.com/shadcn.png" />
                             <AvatarFallback>CN</AvatarFallback>
                         </Avatar>
                     </DropdownMenuTrigger>
-                    <DropdownMenuContent className="w-56">
+                    <DropdownMenuContent className="w-44">
                         <DropdownMenuSeparator />
                         <DropdownMenuGroup>
                             <DropdownMenuItem>
                                 <User className="mr-2 h-4 w-4" />
                                 <span>Profile</span>
-                                <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
                             </DropdownMenuItem>
                             <DropdownMenuItem>
                                 <Bell className="mr-2 h-4 w-4" />
@@ -40,10 +46,9 @@ export default function UserProfileDown({ session }: { session: Session }) {
                             </DropdownMenuItem>
                         </DropdownMenuGroup>
                         <DropdownMenuSeparator />
-                        <DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => handleSignOut}>
                             <LogOut className="mr-2 h-4 w-4" />
                             <span>Log out</span>
-                            <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
                         </DropdownMenuItem>
                     </DropdownMenuContent>
                 </DropdownMenu>
