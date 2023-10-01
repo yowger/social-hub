bd440ea6733f3b459fa46c992b979b50d9ab9196
unknown <rogerpantilyowger@gmail.com>
2023-10-01 11:33:05 +0800

    added cors middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,31 @@
 import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 
-export function middleware(request: NextRequest) {
-    console.log("middleware")
-    return NextResponse.redirect(new URL("/home", request.url))
-}
+const allowedOrigins =
+    process.env.NODE_ENV === "development"
+        ? ["http://localhost:3000"]
+        : ["https://social-hub-seven.vercel.app"]
 
-export const config = {
-    matcher: "/",
+export function middleware(request: NextRequest) {
+    const { pathname } = request.nextUrl
+
+    if (pathname === "/") {
+        return NextResponse.redirect(new URL("/home", request.url))
+    }
+
+    if (pathname.startsWith("/api")) {
+        const origin = request.headers.get("origin")
+
+        if (origin && !allowedOrigins.includes(origin)) {
+            return new NextResponse(null, {
+                status: 400,
+                statusText: "Bad Request",
+                headers: {
+                    "Content-Type": "text/plain",
+                },
+            })
+        }
+
+        return NextResponse.next()
+    }
 }
